480d32fcd18f6ac54125a9d5639417dd442621ca
1taru <109340676+1taru@users.noreply.github.com>
2023-05-21 15:33:35 -0400

    Delete src directory

--- src/dbConnection/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connect() {
-  const mongoUri = 'mongodb://localhost:3000/proyecto';
-
-  console.log('Conexión exitosa a la base de datos');
-}
-
-module.exports = connect;
